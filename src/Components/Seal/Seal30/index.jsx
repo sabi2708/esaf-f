a6f3a42ac85704e59721101781dbf320da2618ed
sabi2708 <mvsabithamanikandan.com>
2025-02-04 19:12:42 +0530

    thirty three

--- src/Components/Seal/Seal30/index.jsx
+++ src/Components/Seal/Seal30/index.jsx
@@ -1,12 +1,8 @@
-import React, { useState } from "react";
+import React, { useRef } from "react";
+import "../Seal30/styles30.css";
 
-import { useEffect } from 'react';
-function Seal30({ branchName }) {
-  useEffect(() => {
-    import('../Seal30/styles30.css');
-  }, []);
-
-  const [name, setName] = useState("");
+function Seal30() {
+  const nameRef = useRef(null);
 
   return (
     <div className="seal30stamp-container">
@@ -14,14 +10,12 @@ function Seal30({ branchName }) {
         className="text seal30dynamic-name"
         contentEditable
         suppressContentEditableWarning
-        onInput={(e) => setName(e.currentTarget.textContent)}
+        ref={nameRef}
       >
-        {name || "Enter name"}
       </p>
       <p className="text seal30designation">Manager</p>
       <p className="text seal30emp-no">Emp. No. 12859</p>
       <p className="text seal30branch">Anakkatty Branch</p>
-      
     </div>
   );
 }
