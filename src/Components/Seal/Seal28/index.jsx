34c65b32e9d1c531de850bf39d627655d1c36e18
sabi2708 <mvsabithamanikandan.com>
2025-02-04 18:27:22 +0530

    twenty four

--- src/Components/Seal/Seal28/index.jsx
+++ src/Components/Seal/Seal28/index.jsx
@@ -1,30 +1,25 @@
 import React, { useState } from "react";
+import "../Seal28/styles28.css";
 
-import { useEffect } from 'react';
-function Seal28({ branchName }) {
-  useEffect(() => {
-    import('../Seal28/styles28.css');
-  }, []);
-
+function Seal28() {
   const [name, setName] = useState("");
 
   return (
-    <div className="seal28box">
+    <div className="seal-container">
       <div className="seal28stamp">
         <span
           className="seal28name"
           contentEditable
           suppressContentEditableWarning
           onInput={(e) => setName(e.currentTarget.textContent)}
+          style={{ display: "inline-block", minWidth: "100px" }}
         >
-          {name || "Enter name"}
+          {name.trim() ? name : ""}
         </span>
         <div className="seal28emp-id">Emp. Id. 12859</div>
       </div>
-     
     </div>
   );
 }
-
 
 export default Seal28;
